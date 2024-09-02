c6b217dc13601b804f158b1d4a4efe631773e746
sara_felix <sarabfelix1@gmail.com>
2024-09-02 17:29:13 -0300

    getters and setters

--- src/main/java/br/com/sara/demohibernate/entity/Student.java
+++ src/main/java/br/com/sara/demohibernate/entity/Student.java
@@ -11,6 +11,39 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
     private String dni;
     private String name;
     private String lastname;
